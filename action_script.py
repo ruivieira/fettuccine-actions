fcbc471aef4fcaafd2a93c8aad2c38c6ee27f572
Rui Vieira <rcardoso@redhat.com>
2023-08-11 00:12:34 +0100

    Add repo location

--- action_script.py
+++ action_script.py
@@ -9,9 +9,10 @@ print("Command-line arguments:", sys.argv)
 # Read the inputs from the environment variables
 sub_action = os.environ.get('INPUT_SUB-ACTION')
 pattern = os.environ.get('INPUT_PATTERN')
+repo = os.environ.get('INPUT_REPO')
 
 def cut_branch():
-    project = Git(".")
+    project = Git(repo)
     new_branch_name = project.branches.create_minor_branch(pattern)
     remote = project._repo.remotes["origin"]
     github_token = os.environ['GITHUB_TOKEN']
